308137302b2ae1e4143a5c3d58aa6b7fe2413f61
Ian McEwen <mian@cyverse.org>
2020-07-29 22:02:42 -0700

    more scaffolding etc

--- src/bulk_typer/core.clj
+++ src/bulk_typer/core.clj
@@ -3,6 +3,10 @@
   (:require [me.raynes.fs :as fs]
             [clj-jargon.init :as init]
             [common-cli.core :as ccli]
+            [clojure-commons.file-utils :as ft]
+            [clojure.tools.logging :as log]
+            [clojure.string :as string]
+            [bulk-typer.irods :as irods]
             [bulk-typer.config :as cfg]
             [service-logging.thread-context :as tc]
     ))
@@ -17,6 +21,7 @@
   []
   [["-c" "--config PATH" "Path to the config file"
     :default "/etc/iplant/de/bulk-typer.properties"]
+   ["-f" "--file PATH" "Path to a file of paths to process"]
    ["-v" "--version" "Print out the version number."]
    ["-h" "--help"]])
 
@@ -42,5 +47,8 @@
       (when-not (fs/readable? (:config options))
         (ccli/exit 1 "The config file is not readable."))
       (cfg/load-config-from-file (:config options))
-      
+      (when (ft/exists? (:file options))
+        (let [files (remove string/blank? (string/split (slurp (:file options)) (re-pattern "\n")))]
+          (log/warn files)
+        ))
       )))
---
+++ src/bulk_typer/irods.clj
@@ -0,0 +1,26 @@
+(ns bulk-typer.irods
+  (:use [clj-jargon.item-ops :only [input-stream]])
+  (:require [heuristomancer.core :as hm]
+            [clojure.tools.logging :as log]
+            [bulk-typer.config :as cfg]))
+
+
+(defn- get-file-type
+  "Uses heuristomancer to determine a the file type of a file."
+  [cm path]
+  (let [result (hm/identify (input-stream cm path) (cfg/filetype-read-amount))]
+    (if-not (nil? result)
+      (name result)
+      result)))
+
+
+(defn content-type
+  "Determines the filetype of path. Reads in a chunk, writes it to a temp file, runs it
+   against the configured script. If the script can't identify it, it's passed to Tika."
+  [cm path]
+
+  (let [info-type (get-file-type cm path)]
+    (log/info "Data object at" path "identified as type" info-type)
+    (if (or (nil? info-type) (empty? info-type))
+      ""
+      info-type)))
