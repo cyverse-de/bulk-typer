032f6016e4caca1e4cb06424985e0bbc564deede
Ian McEwen <mian@cyverse.org>
2021-05-07 11:37:27 -0700

    Use non-snapshot version

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                  [org.cyverse/clojure-commons "2.8.0" :exclusions [commons-logging]]
                  [org.cyverse/service-logging "2.8.2" :exclusions [ch.qos.logback/logback-classic]]
                  [net.logstash.logback/logstash-logback-encoder "4.11"]
-                 [org.cyverse/clj-jargon "3.0.0-SNAPSHOT"
+                 [org.cyverse/clj-jargon "3.0.0"
                    :exclusions [[org.slf4j/slf4j-log4j12]
                                 [log4j]]]
                  [org.cyverse/heuristomancer "2.8.6"]]
